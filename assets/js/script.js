6cb3387dfcdb926c6348d619c2ca1c38d55371e0
Ermiyas B <124329320+Ermi-B@users.noreply.github.com>
2023-04-25 22:11:48 -0500

    more styling and responsivness to heaDER

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,16 @@
 var API_KEY = "f3d3fa0749c3c9ed320b8c28e1721147";
-var bibleVersionID = "de4e12af7f28f599-02";
+var bibleVersionID = "de4e12af7f28f599-02"; //KJV
 var offset = 1;
 var searchText = "I have overcome the world."
 var url = `https://api.scripture.api.bible/v1/bibles/${bibleVersionID}/search?query=${searchText}/`
 
 // Fetch
-fetch(url,{
-    headers: {
-      'api-key': API_KEY
-    }
-})
-.then((data)=>{return data.json()})
-.then((result)=>{
-    console.log(result)
-})
+// fetch(url,{
+//     headers: {
+//       'api-key': API_KEY
+//     }
+// })
+// .then((data)=>{return data.json()})
+// .then((result)=>{
+//     console.log(result)
+// })
