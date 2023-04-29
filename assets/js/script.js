6ef2ee81d467c4bfe1bdc16968958adfd7a3c016
Ermiyas B <124329320+Ermi-B@users.noreply.github.com>
2023-04-29 03:18:01 -0500

    amharic bible search functionality added but having issue with with match not found display

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,44 +6,46 @@ var offset = 1
 var amhURL = 'https://raw.githubusercontent.com/magna25/amharic-bible-json/main/amharic_bible.json' //github url for amharic bible
 
 //SELECTORS
-const inputEl = $('#key-word')
+const inputEl = $('#input')
 const searchBtn = $('#search')
 const searchResult = $('#search-result')  //list element
 const searchContainer = $('#search-container')//list entire div (container)
+var languageSelection = $('#language-selection');
+let resultArray = [] //holds amharic ssearch results
 
+function searchEnglishVerse(){  
+  searchBtn.on('click', function (e) {
+    e.preventDefault()
+    inputEl.attr('required', '')
+    const searchText = inputEl.val()
+    if (
+      searchText === null ||
+      searchText === undefined ||
+      searchText.length <= 0
+    ) {
+      alert('Please provide a key word!')
+    }else {
+      
+      fetch(`https://api.scripture.api.bible/v1/bibles/${bibleVersionID}/search?query=${searchText}/`,
+        {
+          headers: {
+            'api-key': API_KEY,
+          },
+        }
+      )
+        .then((data) => {
+          return data.json()
+        })
+        .then((result) => {
+          displayVerse(result,searchText)
+        })
+  
+    }
+  })
+}
 
-
-searchBtn.on('click', function () {
-  inputEl.attr('required', '')
-  const searchText = inputEl.val()
-  if (
-    searchText === null ||
-    searchText === undefined ||
-    searchText.length <= 0
-  ) {
-    alert('Please provide a key word!')
-  } else {
-    
-    fetch(
-      `https://api.scripture.api.bible/v1/bibles/${bibleVersionID}/search?query=${searchText}/`,
-      {
-        headers: {
-          'api-key': API_KEY,
-        },
-      }
-    )
-      .then((data) => {
-        return data.json()
-      })
-      .then((result) => {
-        getVerse(result,searchText)
-      })
-
-  }
-})
-
-//getVerse function
-function getVerse (result,searchText){
+//display Verse function
+function displayVerse (result,searchText){
 
   const verseInfo = result.data.verses //verseInfo is array of objects   
 
@@ -66,7 +68,7 @@ function getVerse (result,searchText){
       }
       
     }
-    const highlightedContent = arr.join(' '); //convering it back to string          
+    const highlightedContent = arr.join(' '); //converting it back to string          
     const liEl = $("<li>",{
       'class':'w-full bg-white font-bold rounded my-2 px-4 py-1 shadow'
     })
@@ -82,17 +84,123 @@ function getVerse (result,searchText){
 
 
 
-//amharic bible
-fetch(amhURL)
-.then((data)=>{
-    return data.json()
-})
-.then(res=>{
-  console.log(res)
-    console.log(res.books[43].chapters[1].verses[36])   //books = n-1/chapters = n-1/verses = n-1 or n-2...if n is length of array//acts 2:38 = books[43].chapters[1].verses[36]
-    document.body.append(res.books[43].chapters[1].verses[36])
-  })
+
 
   function getAmharicBible(){
+    //amharic bible
+    searchBtn.on('click',function(e){
+      e.preventDefault()
+      const searchText = inputEl.val()
+      fetch(amhURL)
+      .then((data)=>{
+          return data.json()
+      })
+      .then(res=>{
+        console.log(res)
+        searchAmharicVerse(res,searchText)
+        displayAmharicVerse()
+      })
+    })
+     
+  }
+  
+  function searchAmharicVerse(res,searchText){
+    resultArray.length = 0;
+    const books = res.books
+        books.forEach((item)=>{
+          const book = item.title //gENESIS          
+          // console.log(book)
+          const chapters = item.chapters
+          chapters.forEach((item,index)=>{
+            const chapterNumber = item.chapter            
+            const verseContent = item.verses
+            //test variables delete them after test
+            let testVar = 'በዚያን ጊዜ እየታበዩ የሚፎክሩትን ከመካከልሽ አወጣለሁና፥ አንቺም በቅዱስ ተራራዬ ዳግመኛ' //sofonyas 3-11
+            let testVar2 = 'ከዚህ በኋላ ታላቅ ስልጣን ያለው ሌላ መልአክ ከሰማይ ሲወርድ አየሁ፥ ከክብሩም የተነሣ ምድር በራች።' //rev 18 1 
+            let testVar3 = 'ጾርዓን፥ ኤሎንን፥ ኬብሮንን፥ ሠራ።' //1gna zena 10 10 
+            let testVar4 = 'ነጋዴዎችሽን ከሰማይ'
+            let testVar5 = 'ኢየሱስ'
+           
+            //search functionality
+            //reduce
+            
+            const result = verseContent.reduce((acc,curr,index)=>{
+              if(curr.includes(searchText)){
+               return resultArray.push({  //returns an object containing info about the verse match found
+                  'book':book,
+                  'chapter':chapterNumber,
+                  'verse':index+1,
+                  'content':curr
+                })
+              }
+              return acc;
+            },null)
+            if(result!== null){
+              console.log('match found')
+              console.log(resultArray)
+            }else{
+              console.log('not found')
+              matchNotFound();
+            }
+            
+          })
+        })     
+          
+  }
+//language
+ 
+  languageSelection.on('change', changeLanguage);
+  function changeLanguage() {
+    var language = languageSelection.val();
+    console.log(language)
+    if(language =='am'){
+       searchBtn.text('ፈልግ')
+       getAmharicBible()
+    }else if(language =='en'){
+      searchBtn.text('Search')
+      searchEnglishVerse()
+    }
+
+   
+    
+    
+  }
+
+  function displayAmharicVerse(){
+   
+    searchResult.empty()
+    searchContainer.removeClass('hidden') //displays the container for search result
+    
+
+    resultArray.forEach((item)=>{
+      const book = item.book
+      const chapter = item.chapter
+      const verse = item.verse
+      const content = item.content
+      const reference = book + ' '+chapter+':' + verse
+
+     const liEl = $("<li>",{
+      'class':'w-full bg-white font-bold rounded my-2 px-4 py-1 shadow'
+    })
+    const pEl = $("<p>",{'class':'italic text-xs font-light'})
+    pEl.text(content)
+    liEl.text(reference)
+    liEl.append(pEl)
+    searchResult.append(liEl)
+    })
+  }
+
+  function matchNotFound(){
+    searchResult.empty()
+    searchContainer.removeClass('hidden') //displays the container for search result
+
+    const liEl = $("<li>",{
+      'class':'w-full bg-white font-bold rounded my-2 px-4 py-1 shadow'
+    })
+    const pEl = $("<p>",{'class':'italic text-xs font-light'})
+    pEl.text('Try again!')
+    liEl.text('Match not Found')
+    liEl.append(pEl)
+    searchResult.append(liEl)
     
   }
